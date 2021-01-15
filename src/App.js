d369d08b98c42f8c254bbb1b1910de939f6de4eb
George Waller <george.waller3@gmail.com>
2021-01-15 11:01:06 +0000

    Initial commit, working 1x speed clock, no images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+    const [seconds, setSeconds] = useState(0);
+    const hourHand = document.getElementById('hourHand');
+    const minuteHand = document.getElementById('minutesHand');
+
+    useEffect(() => {
+        setInterval(() => setClock(), 1000);
+    }, [])
+
+    const setClock = () => {
+        const currentDate = new Date();
+        const secondsRatio = currentDate.getSeconds() / 60;
+        const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+        const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+        setRotation(hourHand, hoursRatio);
+        setRotation(minuteHand, minutesRatio);
+    }
+
+    const setRotation = (element, rotationRatio) => {
+        try {
+            element.style.setProperty('--rotation', rotationRatio * 360);
+        }
+        catch {
+            console.log(element)
+        }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="clock">
+            <div id="hourHand" className="hand hour" data-hour-hand></div>
+            <div id="minutesHand" className="hand minute" data-minute-hand></div>
+        </div>
     </div>
   );
 }
